c86ea4cc73272e69f3da385fc464ac577db14cb2
YogaRahmahadi <yogarahmahadi1234@gmail.com>
2022-11-15 21:29:02 +0700

    Done Login Page

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import 'package:flutter_starter_stisla/components/background.dart';
 
+import '../register/register.dart';
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
@@ -47,6 +49,73 @@ class LoginScreen extends StatelessWidget {
               child: const TextField(
                 decoration: InputDecoration(
                   labelText: "Password",
+                ),
+                obscureText: true,
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: const Text(
+                "Forgot your password?",
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Color(0xFF2661FA),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(80.0)),
+                  padding: const EdgeInsets.all(0),
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: size.width * 0.5,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(80.0),
+                    gradient: const LinearGradient(colors: [
+                      Color.fromARGB(255, 255, 136, 34),
+                      Color.fromARGB(255, 255, 177, 41),
+                    ]),
+                  ),
+                  padding: const EdgeInsets.all(0),
+                  child: const Text(
+                    "LOGIN",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: GestureDetector(
+                onTap: () => {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RegisterScreen()))
+                },
+                child: const Text(
+                  "Don't Have an Account? Sign Up",
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff2661fa),
+                  ),
                 ),
               ),
             ),
